7f01cdbe5f00d5697825cad4794094573c0e4117
Sami-Jouamaa <122917968+Sami-Jouamaa@users.noreply.github.com>
2024-01-09 20:16:53 -0500

    Add files via upload

---
+++ TP1/client/src/client/Client.java
@@ -0,0 +1,37 @@
+package client;
+import java.io.DataInputStream;
+import java.util.Scanner;
+import java.io.DataOutputStream;
+import java.net.Socket;
+
+public class Client {
+	private static Socket socket;
+	public static void main(String[] args) throws Exception{
+		//Adresse et port du serveur
+		String serverAddress = "127.0.0.1";
+		int port = 5000;
+		
+		//Création d'une nouvelle connexion avec le serveur
+		socket = new Socket(serverAddress, port);
+		System.out.format("Serveur lancé sur [%s:%d]", serverAddress, port);
+		
+		//Création d'un canal entrant pour recevoir les messages envoyés par le serveur
+		DataInputStream in = new DataInputStream(socket.getInputStream());
+		
+		DataOutputStream out = new DataOutputStream(socket.getOutputStream()); //Création de canal d'envoi
+		
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Enter the message you want to send to the server:");
+		String userMessage = scanner.nextLine();
+		
+		out.writeUTF(userMessage); //Envoi de message
+		
+		//Attente de la réception d'un message envoyé par le serveur sur le canal
+		String helloMessageFromServer = in.readUTF();
+		System.out.println(helloMessageFromServer);
+		
+		//fermeture de la connexion avec le serveur
+		socket.close();
+		scanner.close();
+	}
+}
