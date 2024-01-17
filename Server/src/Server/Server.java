059515c229b18ece8a5e202f119ca677a46fa008
Sami-Jouamaa <122917968+Sami-Jouamaa@users.noreply.github.com>
2024-01-16 19:48:14 -0500

    Update Server.java

--- Server/src/Server/Server.java
+++ Server/src/Server/Server.java
@@ -1,37 +1,46 @@
 package Server;
+import Verificateur.Verificateur;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
 import java.net.InetAddress;
 import java.net.InetSocketAddress;
 import java.net.ServerSocket;
+import java.util.Scanner;
 
 public class Server {
 	private static ServerSocket Listener;
+	private static String serverAddress = "127.0.0.1";
+	private static int serverPort = 5000;
 	
 	//Application Serveur
 	public static void main(String[] args) throws Exception {
-		//Compteur incrémenté à chaque connexion d'un client
+		//Compteur incrÃ©mentÃ© Ã  chaque connexion d'un client
 		int clientNumber = 0;
 		
 		//Adresse et port du serveur 
-		String serverAddress = "127.0.0.1";
-		int serverPort = 5000;
 		
-		//Création de la connexion pour communiquer avec les clients
+		serverAddress = Verificateur.askStartAddress();
+		serverPort = Verificateur.askStartPort();
+		
+		//CrÃ©ation de la connexion pour communiquer avec les clients
 		Listener = new ServerSocket();
 		Listener.setReuseAddress(true);
 		InetAddress serverIP = InetAddress.getByName(serverAddress);
 		
-		//Association de l'adresse et du port à la connexion
+		//Association de l'adresse et du port Ã  la connexion
 		Listener.bind(new InetSocketAddress(serverIP, serverPort));
 		System.out.format("The server is running on %s:%d%n", serverAddress, serverPort);
 		
 		try {
-			//À chaque fois qu'un nouveau client se connecte, on exécute la fonction
+			//Ã€ chaque fois qu'un nouveau client se connecte, on exÃ©cute la fonction
 			//run() de l'objet ClientHandler
 			while(true) {
 				//Important: la fonction accept() est bloquante: on attend qu'un prochain client se connecte
-				//On incrémente clientNumber à chaque nouvelle connexion
+				//On incrÃ©mente clientNumber Ã  chaque nouvelle connexion
 				
 				new ClientHandler(Listener.accept(), clientNumber++).start();
+				
 			}
 		}
 		finally {
