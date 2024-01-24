c5a892b117c727927c53e24ce4977250b8e8ce93
Sami-Jouamaa <122917968+Sami-Jouamaa@users.noreply.github.com>
2024-01-23 20:21:44 -0500

    Update ClientHandler.java

--- Server/src/Server/ClientHandler.java
+++ Server/src/Server/ClientHandler.java
@@ -1,29 +1,52 @@
 package Server;
+import Verificateur.Verificateur;
 import java.io.DataOutputStream;
 import java.io.DataInputStream;
 import java.io.IOException;
 import java.net.Socket;
+import java.util.*;
 
 public class ClientHandler extends Thread{
 	private Socket socket;
 	private int clientNumber;
+	private Map<String, String> utilisateurs;
+	
 	public ClientHandler(Socket socket, int clientNumber) {
 		this.socket = socket;
 		this.clientNumber = clientNumber;
+		this.utilisateurs = new TreeMap<String, String>();
 		System.out.println("New connection with client#" + clientNumber + "at" + socket);
+		
+	}
+	
+	public void mapSetter(String utilisateur, String MotDePasse)
+	{
+		utilisateurs.put(utilisateur, MotDePasse);
 	}
 	
 	public void run() {
 		//Création de thread qui envoit un message à un client
 		try {
+			utilisateurs.put("patrice", "1910");
+			
 			DataOutputStream out = new DataOutputStream(socket.getOutputStream()); //Création de canal d'envoi
 			DataInputStream in = new DataInputStream(socket.getInputStream());
+			
+			String userName = in.readUTF();
+			String password = in.readUTF();
+			
+			ArrayList<String> utilisateurMdp = Verificateur.checkLoginInfo(userName, password, utilisateurs);
+			utilisateurs.put(utilisateurMdp.get(0), utilisateurMdp.get(1));
+			
+			System.out.println(utilisateurs.get("sami"));
 			
 			out.writeUTF("Hello from server - you are client#" + clientNumber); //Envoi de message
 			
 			String messageFromClient = in.readUTF();
 			
 			out.writeUTF(messageFromClient);
+			
+			
 		}
 		catch(IOException e){
 			System.out.println("Error handling client#" + clientNumber + ":" + e);
