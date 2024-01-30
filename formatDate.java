4e1f7888f7af48bc69d38377dfcbb3b5d13236bf
BereniceDubois <97747070+BereniceDubois@users.noreply.github.com>
2024-01-30 18:41:35 -0500

    Update formatDate.java

--- formatDate.java
+++ formatDate.java
@@ -1,26 +1,29 @@
-// Online Java Compiler
-// Use this editor to write, compile and run your Java code online
-
 import java.time.format.DateTimeFormatter; 
 import java.time.LocalDateTime;
 
-class HelloWorld {
+class Server {
     public static void main(String[] args) {
-
-        
+        afficherMessage("Bere","127.01.01",5050,"Hello World");
     }
-    public static void afficherUtilisateur(){
-        
-    }
-    public static void afficherDate(){
+    
+    public static String getDate(){
         LocalDateTime d = LocalDateTime.now();
         
         DateTimeFormatter formateur = DateTimeFormatter.ofPattern("dd-MM-yyyy'@'HH:mm:ss");
 
         String dateFormatee = d.format(formateur);
 
-        System.out.println(dateFormatee);
+        return dateFormatee;
 
         //adapte de https://www.w3schools.com/java/java_date.asp
+    }
+    
+    public static void afficherMessage(String utilisateur, String adresse, int port, String message)
+    {
+        String portString = String.valueOf(port);
+        String date = getDate();
+        String affichage = "[" + utilisateur+"-"+adresse+":"+portString+"-"+date+"]:"+message;
+        System.out.println(affichage);
+        
     }
 }
