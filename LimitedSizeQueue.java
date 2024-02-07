fd10df683565fafc07b51941ce8bbfe679c1c8eb
Sami-Jouamaa <122917968+Sami-Jouamaa@users.noreply.github.com>
2024-02-06 20:35:07 -0500

    Update LimitedSizeQueue.java

--- LimitedSizeQueue.java
+++ LimitedSizeQueue.java
@@ -2,19 +2,34 @@ package Server;
 import java.util.LinkedList;
 import java.util.Queue;
 
-public class LimitedSizeQueue<T> extends LinkedList<T> {
-    private final int maxSize;
+public class LimitedSizeQueue<E> //classe reprise de https://www.geeksforgeeks.org/how-to-implement-size-limited-queue-that-holds-last-n-elements-in-java/
+extends LinkedList<E> { 
 
-    public LimitedSizeQueue(int maxSize) {
-        this.maxSize = maxSize;
-    }
-
-    @Override
-    public boolean add(T element) {
-        if (size() >= maxSize) {
-            // Remove the oldest element if the queue is full
-            super.remove();
-        }
-        return super.add(element);
-    }
+	// Variable which store the 
+	// SizeLimitOfQueue of the queue 
+	private int SizeLimitOfQueue; 
+  
+	// Constructor method for initializing 
+	// the SizeLimitOfQueue variable 
+	public LimitedSizeQueue(int SizeLimitOfQueue) 
+	{ 
+		this.SizeLimitOfQueue = SizeLimitOfQueue; 
+	} 
+  
+	// Override the method add() available 
+	// in LinkedList class so that it allow 
+	// addition  of element in queue till 
+	// queue size is less than 
+	// SizeLimitOfQueue otherwise it remove 
+	// the front element of queue and add 
+	// new element 
+	@Override
+	public boolean add(E o) 
+	{ 
+		while (this.size() == SizeLimitOfQueue) { 
+			super.remove(); 
+		} 
+		super.add(o); 
+		return true; 
+	} 
 }
