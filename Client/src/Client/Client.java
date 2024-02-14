f12ffa2f7b018482c64ef3265700d9a8d76cc49c
Sami-Jouamaa <122917968+Sami-Jouamaa@users.noreply.github.com>
2024-02-14 14:53:57 -0500

    Update Client.java

--- Client/src/Client/Client.java
+++ Client/src/Client/Client.java
@@ -14,39 +14,37 @@ public class Client extends Verificateur {
 	private static ArrayList<String> infoConnections = new ArrayList<>();
 	
 	public static void main(String[] args) throws Exception{
-		//Adresse et port du serveur
-
+		//On demande l'adresse et le port du serveur, ensuite, on les vérifie. Après la vérification, on connecte le client
 		serverAddress = Verificateur.askStartAddress();
 		serverPort = Verificateur.askStartPort();
 		socket = new Socket(serverAddress, serverPort);
 		
+		//Récupère le nom d'utilisateur et le mot de passe dans une arrayList de string
 		infoConnections = Verificateur.askLoginInfo();
 		
 		DataOutputStream out = new DataOutputStream(socket.getOutputStream()); //Création de canal d'envoi
 		DataInputStream in = new DataInputStream(socket.getInputStream());
 		
+		//Envoi des informations de l'utilisateur au Serveur pour qu'il nous laisse nous connecter à la salle de clavardage
 		out.writeUTF(infoConnections.get(0));
 		out.writeUTF(infoConnections.get(1));
 		
 		//Création d'une nouvelle connexion avec le serveur
-		
 		System.out.format("Serveur lancé sur [%s:%d] \n", serverAddress, serverPort);
 		
+		//Message de bienvenue du serveur
 		String helloMessageFromServer = in.readUTF();
 		System.out.println(helloMessageFromServer);
 		
-		Scanner scanner = new Scanner(System.in);
-		
 		System.out.println(in.readUTF());
-		//do {
-			//tempMessage = in.readUTF();
-			//System.out.println(tempMessage);
-		//}while(tempMessage != "");
 		
 		System.out.println("Type EXIT to leave the chatroom");
 		System.out.println("Enter the message you want to send to the server: \n");
-		String userMessage = "";
 		
+		String userMessage = "";
+		Scanner scanner = new Scanner(System.in);
+		
+		//Instanciation d'un objet pour la récupération des messages des autres utilisateurs
 		new ServerHandler(in).start();
 		
 		while (true)
@@ -56,6 +54,7 @@ public class Client extends Verificateur {
 				userMessage = scanner.nextLine();
 				if (userMessage.equals("EXIT"))
 				{
+					///Fermeture de la connexion du client au serveur
 					socket.close();
 					scanner.close();
 					System.exit(0);
@@ -63,8 +62,7 @@ public class Client extends Verificateur {
 				}
 				else
 				{
-					out.writeUTF(userMessage); //Envoi de message
-
+					out.writeUTF(userMessage); //Envoi de message en boucle tant que le client en écrit
 				}
 			}
 			catch(IOException e)
