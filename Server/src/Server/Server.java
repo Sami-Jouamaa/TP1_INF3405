b4dbdb69ad26248baa5fa8411e32b2db756fcc37
Sami-Jouamaa <122917968+Sami-Jouamaa@users.noreply.github.com>
2024-02-14 14:54:39 -0500

    Update Server.java

--- Server/src/Server/Server.java
+++ Server/src/Server/Server.java
@@ -1,15 +1,10 @@
 package Server;
 import Verificateur.Verificateur;
-import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
-import java.nio.file.*;
 import java.net.InetAddress;
 import java.net.InetSocketAddress;
 import java.net.ServerSocket;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.util.Scanner;
 import java.util.*;
 import java.io.File;
 import java.io.FileWriter;
@@ -17,25 +12,21 @@ import java.io.FileWriter;
 public class Server extends Verificateur {
 	public static int queueSize = 15;
 	public static ServerSocket Listener;
-	public static String serverAddress = "127.0.0.1";
-	public static int serverPort = 5000;
+	public static String serverAddress ;
+	public static int serverPort;
 	public static Map <String, String> utilisateurs = new TreeMap<String, String>();
 	public static LimitedSizeQueue<String> messages = new LimitedSizeQueue(queueSize);
-	public static ArrayList<DataOutputStream> tempSocketsList = new ArrayList<DataOutputStream>();
+	public static ArrayList<DataOutputStream> dataOutputStreamsList = new ArrayList<DataOutputStream>();
 	public static FileWriter writerUtilisateurs;
 	
 	//Application Serveur
 	public static void main(String[] args) throws Exception {
 		int clientNumber = 0;
+		//Création des fichiers de sauvegarde s'ils n'existent pas
 		createFile("sauvegarde.txt");
 		createFile("utilisateurs.txt");		
 		
-		//Compteur incrémenté à chaque connexion d'un client
-		
-		//messages.add("anisdfnisadngfidng");
-		//messages.add("salsatidkgmjfkgmnfknhfkhnfkhnkhnfkh");
-		//utilisateurs.put("Patrice",  "1234");
-		
+		//Morceaux de code exécuté à la fermeture du serveur
 		Runtime.getRuntime().addShutdownHook(new Thread(){
 				public void run()
 				{
@@ -44,26 +35,27 @@ public class Server extends Verificateur {
 						FileWriter writerUtilisateurs = new FileWriter("utilisateurs.txt");
 						FileWriter writerMessages = new FileWriter("sauvegarde.txt");
 						
+						//Enregistrement des utilisateurs
 						for (String key : utilisateurs.keySet())
 						{
 							writerUtilisateurs.write(key + '\t' + utilisateurs.get(key) + '\n');
 						}
 						
+						//Enregistrement des messages
 						for (int i = 0; i < messages.size(); i++) 
 						{
 							writerMessages.write(messages.get(i) + '\n');
-							
 						}
+						
 						writerUtilisateurs.close();
 						writerMessages.close();
-					
 					} catch (IOException e1) {
 						e1.printStackTrace();
 					}
 				}
 			});
 		
-		//Adresse et port du serveur 
+		//Demande et vérification adresse et port du serveur 
 		serverAddress = Verificateur.askStartAddress();
 		serverPort = Verificateur.askStartPort();
 		
@@ -77,13 +69,9 @@ public class Server extends Verificateur {
 		System.out.format("The server is running on %s:%d%n", serverAddress, serverPort);
 		
 		try {
-			//À chaque fois qu'un nouveau client se connecte, on exécute la fonction
-			//run() de l'objet ClientHandler
-			
 			while(true) {
 				//Important: la fonction accept() est bloquante: on attend qu'un prochain client se connecte
 				//On incrémente clientNumber à chaque nouvelle connexion
-				
 				new ClientHandler(Listener.accept(), clientNumber++).start();
 			}
 		}
@@ -93,8 +81,10 @@ public class Server extends Verificateur {
 		}
 	}
 	
+	
 	public static void createFile(String name)
 	{
+		//Méthode qui crée un fichier, à partir du nom entré, s'il n'existe pas
 		try
 		{
 			File f = new File(name);
@@ -111,17 +101,6 @@ public class Server extends Verificateur {
 		{
 			System.out.println("Error occurred");
 		}
-		
-	}
-	
-	public static void addToQueue(String message)
-	{
-		messages.add(message);
-	}
-	
-	public static void addUserInfoToTree(String username, String password)
-	{
-		utilisateurs.put(username, password);
 	}
 }
 
